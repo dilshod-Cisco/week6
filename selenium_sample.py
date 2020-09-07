503e7139cbfed34b67deef2044e22cdfccd92b3c
Dilshod <dilshodanvarov@gmail.com>
2020-09-07 01:07:02 -0400

    added README file

---
+++ selenium_sample.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome() #
+# or specify the path: driver = webdriver.Chrome('/path/to/chromedriver')
+driver.get("https://google.com")
+print("opened the browser and google website")
+time.sleep(3)
+
+
+search_text_box = driver.find_element_by_name("q")
+print("identified google search box")
+time.sleep(3)
+
+search_text_box.clear()
+search_text_box.send_keys("python selenium ")
+print("cleared the search box the typed 'python selenium' words in it")
+time.sleep(3)
+
+search_text_box.send_keys(Keys.RETURN)
+print("hit the inter button")
+time.sleep(3)
+
+print("now closing the browser  .....")
+driver.close()
